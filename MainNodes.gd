7b12d92da84f24c11aeaa7f5b0bc8755ec1e2eb3
Kubafaw <95341964+Kubafaw@users.noreply.github.com>
2026-08-24 12:56:32 +0200

    Update Redo
    File cleanup

--- MainNodes.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var _GameN : GameNode = null
-var _MainM : MainMap = null
-var _HUD : HUD = null
-var _MPlayer : Player = null
-
-func _ready() -> void:
-	print(_GameN, _MainM, _HUD, _MPlayer)
